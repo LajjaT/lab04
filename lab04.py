f8a663e383fb77e277e1c40d77a6e1266b91da57
unknown <lajja.tharwala@ontariotechu.net>
2023-10-17 08:46:38 -0400

    Lab 04 completed code

--- lab04.py
+++ lab04.py
@@ -4,12 +4,18 @@ def main():
     names = ['Lucie Manette', 'Charles Darnay', 'Sydney Carton']
     test1_marks = [34.0, 75.5, 58.0]
     test2_marks = [47.5, 82.0, 63.5]
-
+    
     student_data = []
 
-    # TODO: YOUR CODE TO CREATE THE LIST OF DICTIONARIES GOES HERE
-
+    for i in range(len(names)):
+        student_dictionary = {}
+        full_names = names[i].split()
+        student_dictionary = {'first_name': full_names[0],'last_name': full_names[1],'test1': test1_marks[i],'test2': test2_marks[i]}
+        student_data.append(student_dictionary)
+    
     # TODO: YOUR CODE TO DELETE THE LAST CHARACTER GOES HERE
+
+    student_data.pop(-1)
 
     print(student_data)
 
@@ -23,8 +29,23 @@ def main():
         'level': 9,
     }
 
-    # TODO: YOUR CODE TO OUTPUT A SUMMARY FOR THIS CHARACTER GOES HERE
+    name = ""
+    name = character['name']
 
+    race = ""
+    race = character['race']
+
+    clss = ""
+    clss = character['class']
+
+    hp = ""
+    hp = character['hp']
+
+    level = ""
+    level = character['level']
+
+    # TODO: YOUR CODE TO OUTPUT A SUMMARY FOR THIS CHARACTER GOES HERE
+    print(f'{name} (level {level} {race} {clss}) - HP: {hp}')
 
 if __name__ == "__main__":
     main()
